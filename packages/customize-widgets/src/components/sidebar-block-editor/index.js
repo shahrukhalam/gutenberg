2173bd50493630e172429639822b8c7d423fd651
Daniel Richards <daniel.richards@automattic.com>
2021-04-19 15:09:55 +0800

    Implement media uploading

--- packages/customize-widgets/src/components/sidebar-block-editor/index.js
+++ packages/customize-widgets/src/components/sidebar-block-editor/index.js
@@ -1,6 +1,13 @@
 /**
+ * External dependencies
+ */
+import { defaultTo } from 'lodash';
+
+/**
  * WordPress dependencies
  */
+import { store as coreStore } from '@wordpress/core-data';
+import { useSelect } from '@wordpress/data';
 import { useMemo, createPortal } from '@wordpress/element';
 import {
 	BlockList,
@@ -16,6 +23,7 @@ import {
 	SlotFillProvider,
 	Popover,
 } from '@wordpress/components';
+import { uploadMedia } from '@wordpress/media-utils';
 
 /**
  * Internal dependencies
@@ -25,14 +33,35 @@ import Header from '../header';
 import useInserter from '../inserter/use-inserter';
 import SidebarEditorProvider from './sidebar-editor-provider';
 
-export default function SidebarBlockEditor( { sidebar, inserter, inspector } ) {
+export default function SidebarBlockEditor( {
+	blockEditorSettings,
+	sidebar,
+	inserter,
+	inspector,
+} ) {
 	const [ isInserterOpened, setIsInserterOpened ] = useInserter( inserter );
-	const settings = useMemo(
-		() => ( {
-			__experimentalSetIsInserterOpened: setIsInserterOpened,
-		} ),
+	const hasUploadPermissions = useSelect(
+		( select ) =>
+			defaultTo( select( coreStore ).canUser( 'create', 'media' ), true ),
 		[]
 	);
+	const settings = useMemo( () => {
+		let mediaUploadBlockEditor;
+		if ( hasUploadPermissions ) {
+			mediaUploadBlockEditor = ( { onError, ...argumentsObject } ) => {
+				uploadMedia( {
+					wpAllowedMimeTypes: blockEditorSettings.allowedMimeTypes,
+					onError: ( { message } ) => onError( message ),
+					...argumentsObject,
+				} );
+			};
+		}
+
+		return {
+			__experimentalSetIsInserterOpened: setIsInserterOpened,
+			mediaUpload: mediaUploadBlockEditor,
+		};
+	}, [] );
 	const parentContainer = document.getElementById(
 		'customize-theme-controls'
 	);
